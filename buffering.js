699133941340a205bfabec91c2e1d91fe0d9439f
elecash <elecash@gmail.com>
2014-10-19 11:51:03 +0200

    release 0.6.1

--- buffering.js
+++ buffering.js
@@ -1,29 +1,19 @@
 /**
- * @license Videogular v0.6.0 http://videogular.com
+ * @license Videogular v0.6.1 http://videogular.com
  * Two Fucking Developers http://twofuckingdevelopers.com
  * License: MIT
  */
 "use strict";
 /**
  * @ngdoc directive
- * @name com.2fdevs.videogular.plugins.buffering:buffering
+ * @name com.2fdevs.videogular.plugins.buffering:vgBuffering
  * @restrict E
  * @description
  * Shows a spinner when Videogular is buffering or preparing the video player.
  *
  * ```html
- * <videogular vg-width="config.width"
- *        vg-height="config.height"
- *        vg-theme="config.theme.url"
- *        vg-autoplay="config.autoPlay"
- *        vg-stretch="config.stretch.value"
- *        vg-responsive="config.responsive">
- *    <video preload='metadata'>
- *        <source src="assets/videos/videogular.mp4" type="video/mp4">
- *        <source src="assets/videos/videogular.webm" type="video/webm">
- *
- *    <track kind="captions" src="assets/subs/pale-blue-dot.vtt" srclang="en" label="English" default></track>
- *    </video>
+ * <videogular vg-theme="config.theme.url" vg-autoplay="config.autoPlay">
+ *    <vg-video vg-src="sources"></vg-video>
  *
  *    <vg-buffering></vg-buffering>
  * </videogular>
@@ -78,7 +68,6 @@ angular.module("com.2fdevs.videogular.plugins.buffering", [])
 							return API.isReady;
 						},
 						function (newVal, oldVal) {
-							console.log(newVal + " != " + oldVal);
 							if (newVal != oldVal) {
 								onPlayerReady(newVal);
 							}
