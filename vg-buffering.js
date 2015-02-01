0ea12b3c41dd2fc02b867c395f22ffe820644bf2
elecash <elecash@gmail.com>
2015-02-01 18:57:43 +0100

    Release v1.0.0

--- vg-buffering.js
+++ vg-buffering.js
@@ -1,18 +1,18 @@
 /**
- * @license Videogular v0.7.2 http://videogular.com
+ * @license Videogular v1.0.0 http://videogular.com
  * Two Fucking Developers http://twofuckingdevelopers.com
  * License: MIT
  */
 /**
  * @ngdoc directive
- * @name com.2fdevs.videogular.plugins.buffering:vgBuffering
+ * @name com.2fdevs.videogular.plugins.buffering.directive:vgBuffering
  * @restrict E
  * @description
  * Shows a spinner when Videogular is buffering or preparing the video player.
  *
  * ```html
  * <videogular vg-theme="config.theme.url" vg-autoplay="config.autoPlay">
- *    <vg-video vg-src="sources"></vg-video>
+ *    <vg-media vg-src="sources"></vg-media>
  *
  *    <vg-buffering></vg-buffering>
  * </videogular>
@@ -21,52 +21,60 @@
  */
 "use strict";
 angular.module("com.2fdevs.videogular.plugins.buffering", [])
+  .run(
+    ["$templateCache", function($templateCache) {
+      $templateCache.put("vg-templates/vg-buffering",
+        '<div class="bufferingContainer">\
+          <div ng-class="spinnerClass" class="loadingSpinner"></div>\
+        </div>');
+    }]
+  )
 	.directive(
 	"vgBuffering",
 	["VG_STATES", "VG_UTILS", function (VG_STATES, VG_UTILS) {
 		return {
 			restrict: "E",
 			require: "^videogular",
-			template: "<div class='bufferingContainer'>" +
-				"<div ng-class='spinnerClass' class='loadingSpinner'></div>" +
-				"</div>",
+      templateUrl: function(elem, attrs) {
+        return attrs.vgTemplate || 'vg-templates/vg-buffering';
+      },
 			link: function (scope, elem, attr, API) {
-				function showSpinner() {
+        scope.showSpinner = function showSpinner() {
 					scope.spinnerClass = {stop: API.isBuffering};
 					elem.css("display", "block");
-				}
+				};
 
-				function hideSpinner() {
+        scope.hideSpinner = function hideSpinner() {
 					scope.spinnerClass = {stop: API.isBuffering};
 					elem.css("display", "none");
-				}
+				};
 
-				function setState(isBuffering) {
+        scope.setState = function setState(isBuffering) {
 					if (isBuffering) {
-						showSpinner();
+            scope.showSpinner();
 					}
 					else {
-						hideSpinner();
+            scope.hideSpinner();
 					}
-				}
+				};
 
-				function onStateChange(state) {
+        scope.onStateChange = function onStateChange(state) {
 					if (state == VG_STATES.STOP) {
-            hideSpinner();
+            scope.hideSpinner();
 					}
-				}
+				};
 
-				function onPlayerReady(isReady) {
+        scope.onPlayerReady = function onPlayerReady(isReady) {
 					if (isReady) {
-						hideSpinner();
+            scope.hideSpinner();
 					}
-				}
+				};
 
-				showSpinner();
+        scope.showSpinner();
 
 				// Workaround for issue #16: https://github.com/2fdevs/videogular/issues/16
 				if (VG_UTILS.isMobileDevice()) {
-					hideSpinner();
+          scope.hideSpinner();
 				}
 				else {
 					scope.$watch(
@@ -75,7 +83,7 @@ angular.module("com.2fdevs.videogular.plugins.buffering", [])
 						},
 						function (newVal, oldVal) {
 							if (API.isReady == true || newVal != oldVal) {
-								onPlayerReady(newVal);
+                scope.onPlayerReady(newVal);
 							}
 						}
 					);
@@ -87,7 +95,7 @@ angular.module("com.2fdevs.videogular.plugins.buffering", [])
 					},
 					function (newVal, oldVal) {
 						if (newVal != oldVal) {
-							onStateChange(newVal);
+              scope.onStateChange(newVal);
 						}
 					}
 				);
@@ -98,7 +106,7 @@ angular.module("com.2fdevs.videogular.plugins.buffering", [])
 					},
 					function (newVal, oldVal) {
 						if (newVal != oldVal) {
-							setState(newVal);
+              scope.setState(newVal);
 						}
 					}
 				);
